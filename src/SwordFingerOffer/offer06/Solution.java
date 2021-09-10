8d3332c3a4a1ce9fd5dc0d2c027de19f97e1804f
小鹏 <981077525@qq.com>
2021-09-10 21:59:51 +0800

    剑指 Offer 06. 从尾到头打印链表

---
+++ src/SwordFingerOffer/offer06/Solution.java
@@ -0,0 +1,39 @@
+package SwordFingerOffer.offer06;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    // 通过栈Stack来实现
+    public static int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        int size = 0;
+        for(ListNode i = head; i!= null;i = i.next){
+            stack.push(i.val);
+            size++;
+        }
+        int[] arr = new int[size];
+        for(int i = 0;i<size;i++){
+            arr[i] = stack.pop();
+        }
+
+        return arr;
+    }
+}
+
+
---
+++ src/problem/leet014/Solution.java
@@ -0,0 +1,12 @@
+package problem.leet014;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String same = "";
+
+        char c = 'a';
+
+
+        return same;
+    }
+}
