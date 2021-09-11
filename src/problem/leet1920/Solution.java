54e99938e885a3af844b67b623f7b59d11dacc16
小鹏 <981077525@qq.com>
2021-09-11 22:13:42 +0800

    LeetCode 1920. 基于排列构建数组

---
+++ src/problem/leet1920/Solution.java
@@ -0,0 +1,13 @@
+package problem.leet1920;
+
+public class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for(int i = 0;i<ans.length;i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
+
