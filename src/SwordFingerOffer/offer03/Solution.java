cdc9e4e5f111faa2d3861752eaa46f5fa0b81ce7
小鹏 <981077525@qq.com>
2021-09-11 11:54:20 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ src/SwordFingerOffer/offer03/Solution.java
@@ -0,0 +1,27 @@
+package SwordFingerOffer.offer03;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+    public int findRepeatNumber(int[] nums){
+        Map<Integer,Integer> map = new HashMap<>() ;
+
+        for(int i = 0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.replace(nums[i], map.get(nums[i]) + 1);
+            }else {
+                map.putIfAbsent(nums[i], 1);
+            }
+        }
+
+        for(int i : map.keySet()){
+            if(map.get(i) > 1){
+                return i;
+            }
+        }
+        // 不存在返回 -1
+        return -1;
+    }
+}
