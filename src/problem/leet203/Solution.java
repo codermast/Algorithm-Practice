50604ae991eb35aabca9f4884abf247f5aecc7b4
小鹏 <981077525@qq.com>
2021-09-12 19:14:43 +0800

    LeetCode 203. 移除链表元素

---
+++ src/problem/leet203/Solution.java
@@ -0,0 +1,27 @@
+package problem.leet203;
+
+//https://leetcode-cn.com/problems/remove-linked-list-elements/
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode header = new ListNode(-1);
+        header.next = head;
+        ListNode cur = header;
+        while(cur.next != null){
+            if(cur.next.val == val ){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+        return header.next;
+    }
+}
