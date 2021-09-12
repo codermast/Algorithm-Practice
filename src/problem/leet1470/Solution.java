29a4a027d2e7ee547b40df660aef54210d797ea0
小鹏 <981077525@qq.com>
2021-09-12 12:18:13 +0800

    LeetCode 1470. 重新排列数组

---
+++ src/problem/leet1470/Solution.java
@@ -0,0 +1,20 @@
+package problem.leet1470;
+
+public class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ret = new int[nums.length];
+
+        int index = 0;
+        for(int i = 0;i<n;i++){
+            ret[index] = nums[i];
+            index += 2;
+        }
+        index = 1;
+        for(int i = n;i<n * 2;i++){
+            ret[index] = nums[i];
+            index += 2;
+        }
+
+        return ret;
+    }
+}
