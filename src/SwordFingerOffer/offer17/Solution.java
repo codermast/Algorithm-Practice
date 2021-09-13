8cc77f4a92ad3b074cf0c8683dc0e7bbe2089731
小鹏 <981077525@qq.com>
2021-09-13 20:18:54 +0800

    剑指 Offer 17. 打印从1到最大的n位数

--- src/SwordFingerOffer/offer17/Solution.java
+++ src/SwordFingerOffer/offer17/Solution.java
@@ -1,7 +1,15 @@
 package SwordFingerOffer.offer17;
 
+//https://leetcode-cn.com/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/
+
 public class Solution {
     public int[] printNumbers(int n) {
+        int target = (int) (Math.pow(10,n) - 1);
+        int[] arr = new int[target];
 
+        for(int i = 1;i<=target;i++){
+            arr[i - 1] = i;
+        }
+        return arr;
     }
 }
