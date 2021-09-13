5cbdd27c394b3080eae0673218b3fd49b0e5dff7
小鹏 <981077525@qq.com>
2021-09-13 21:31:00 +0800

    剑指 Offer 22. 链表中倒数第k个节点

--- src/SwordFingerOffer/offer22/Solution.java
+++ src/SwordFingerOffer/offer22/Solution.java
@@ -1,4 +1,24 @@
 package SwordFingerOffer.offer22;
 
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+
 public class Solution {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+
+        int length = 0;
+        for(ListNode i = head;i!=null;i = i.next){
+            length++;
+        }
+
+        for(int i = 0;i<length - k;i++){
+            head = head.next;
+        }
+
+        return head;
+    }
 }
