06f67f424dd1de27d0e53bcbbb6121c7f7f0cfc7
小鹏 <981077525@qq.com>
2021-09-13 20:44:09 +0800

    剑指 Offer 18. 删除链表的节点

---
+++ src/SwordFingerOffer/offer18/Solution.java
@@ -0,0 +1,28 @@
+package SwordFingerOffer.offer18;
+
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution {
+     public ListNode deleteNode(ListNode head, int val) {
+        // 如果根节点就是要删除的结点直接返回根节点的下一个结点
+         if(head.val == val){
+             return head.next;
+         }
+
+         ListNode i = head;
+         ListNode prev = head;
+         while(i != null){
+             if(i.val == val){
+                prev.next = i.next;
+             }
+             prev = i;
+             i = i.next;
+         }
+        return head;
+     }
+
+}
