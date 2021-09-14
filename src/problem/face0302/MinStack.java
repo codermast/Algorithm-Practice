6b208e310b273f60842792467187e13c125313d1
小鹏 <981077525@qq.com>
2021-09-14 15:41:25 +0800

    面试题 03.02. 栈的最小值

---
+++ src/problem/face0302/MinStack.java
@@ -0,0 +1,40 @@
+package problem.face0302;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if(minStack.empty()){
+            minStack.push(x);
+        }else{
+            if(x<getMin()){
+                minStack.push(x);
+            }else{
+                minStack.push(getMin());
+            }
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
