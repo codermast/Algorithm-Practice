742536c3bd5b6db5663151009d8e5fe6de9421d9
小鹏 <981077525@qq.com>
2021-09-14 21:03:02 +0800

    LeetCode 202. 快乐数

---
+++ src/problem/leet202/Solution.java
@@ -0,0 +1,24 @@
+package problem.leet202;
+
+public class Solution {
+    public boolean isHappy(int n) {
+        int num = n;
+        while(num > 4){
+            int sum = 0;
+
+            while(num > 9){
+                sum += Math.pow(num%10,2);
+                num /= 10;
+            }
+            sum += Math.pow(num,2);
+            num = sum;
+            continue;
+        }
+
+        if(num == 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
