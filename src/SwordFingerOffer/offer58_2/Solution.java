4572ab8105201c7cba944e9df8df2366d7c1f229
小鹏 <981077525@qq.com>
2021-09-17 21:28:55 +0800

    剑指 Offer 58 - II. 左旋转字符串

--- src/SwordFingerOffer/offer58_2/Solution.java
+++ src/SwordFingerOffer/offer58_2/Solution.java
@@ -1,5 +1,6 @@
 package SwordFingerOffer.offer58_2;
 
+// https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
 public class Solution {
     public String reverseLeftWords(String s, int n) {
         String str1 = s.substring(0,n);
