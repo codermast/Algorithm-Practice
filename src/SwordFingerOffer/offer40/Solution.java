874a919384487a16257c286bedecb6213c53c72f
小鹏 <981077525@qq.com>
2021-09-22 21:10:04 +0800

    剑指 Offer 40. 最小的k个数

--- src/SwordFingerOffer/offer40/Solution.java
+++ src/SwordFingerOffer/offer40/Solution.java
@@ -2,6 +2,10 @@ package SwordFingerOffer.offer40;
 
 import java.util.Arrays;
 // https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+
+/*
+*   使用冒泡排序，然后进行数组的指定复制
+* */
 public class Solution {
     public int[] getLeastNumbers(int[] arr, int k) {
         for (int i = 0; i < arr.length - 1; i++) {
