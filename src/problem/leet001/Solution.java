3dcbf37bb4c48897ffe7aa100cf9a98fca935537
小鹏 <981077525@qq.com>
2021-11-24 21:14:09 +0800

    Leetcode 1. 两数之和

--- src/problem/leet001/Solution.java
+++ src/problem/leet001/Solution.java
@@ -3,7 +3,6 @@ package problem.leet001;
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ret = new int[2];
-
         for(int i = 0;i<nums.length ;i++){
             for(int j = i + 1 ; j<nums.length; j++){
                 if(nums[i] + nums[j] == target){
