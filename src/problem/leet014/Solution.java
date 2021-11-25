8c88b215cf98dc7318d52c45d11fdd2a72b3e932
小鹏 <981077525@qq.com>
2021-11-25 21:33:43 +0800

    Leetcode 14. 最长公共前缀

--- src/problem/leet014/Solution.java
+++ src/problem/leet014/Solution.java
@@ -2,11 +2,31 @@ package problem.leet014;
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String same = "";
+        int min = strs[0].length();
+        for(String s : strs){
+            if(s.length() < min){
+                min = s.length();
+            }
+        }
 
-        char c = 'a';
+        StringBuilder sb = new StringBuilder();
 
+        for(int i = 0;i<min;i++){
+            char c = strs[0].charAt(i);
+            for(String s : strs){
+                if(s.charAt(i) != c){
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 
-        return same;
+    public static void main(String[] args) {
+        String[] arr = {"flower","flower","flower","flower"};
+        Solution s = new Solution();
+
+        System.out.println(s.longestCommonPrefix(arr));
     }
 }
