27af1736a21b7b4cb17e4896bc588ddfe93ecd22
小鹏 <981077525@qq.com>
2021-11-25 20:52:10 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

--- src/SwordFingerOffer/offer35_02/Solution.java
+++ src/SwordFingerOffer/offer35_02/Solution.java
@@ -1,21 +1,18 @@
 package SwordFingerOffer.offer35_02;
 
-public class Solution {
+class Solution {
     public int missingNumber(int[] nums) {
         if(nums[0] != 0){
             return 0;
         }
-
         int number = 0;
-        for(int i = 0;i<nums.length;i++){
-             if(nums[i] != number){
-                return nums[i] - 1;
+        for(int i:nums){
+            if(i != number){
+                return i - 1;
             }
             number ++;
         }
-
         return nums[nums.length - 1] + 1;
-
     }
     /*
     *   测试用例
