da5129aff41a488f8aa12d104e2c35b86db8a6e7
小鹏 <981077525@qq.com>
2021-11-27 22:02:13 +0800

    Leetcode 217. 存在重复元素

---
+++ src/problem/leet217/Solution.java
@@ -0,0 +1,19 @@
+package problem.leet217;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int n : nums){
+            if(map.containsKey(n)){
+                return true;
+            }else{
+                map.put(n,1);
+            }
+        }
+        return false;
+    }
+}
