215f0c0da9fd6392f3220b3f728f24c20a87daae
小鹏 <981077525@qq.com>
2021-12-10 15:35:20 +0800

    Leetcode 234. 回文链表

---
+++ src/problem/leet234/Solution.java
@@ -0,0 +1,42 @@
+package problem.leet234;
+
+import java.util.Stack;
+
+public class Solution {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 回文链表
+    public boolean isPalindrome(ListNode head) {
+        // 回文链表
+        public boolean isPalindrome (ListNode head){
+            Stack<Integer> stack = new Stack<>();
+
+            for (ListNode i = head; i != null; i = i.next) {
+                stack.push(i.val);
+            }
+
+            for (ListNode i = head; i != null; i = i.next) {
+                if (i.val != stack.pop()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
