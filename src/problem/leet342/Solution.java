8baf894cb3e278a9618d754d35bad1ed856ebb61
小鹏 <981077525@qq.com>
2021-12-12 10:39:34 +0800

    Leetcode 709. 转换成小写字母

---
+++ src/problem/leet342/Solution.java
@@ -0,0 +1,26 @@
+//package problem.leet342;
+//
+//public class Solution {
+//    public static boolean isPowerOfFour(int n) {
+//        for(int i = 0;i<32;i++){
+//            if((n & 1) == 1 && (i + 1) % 2 == 0{
+//                n >>= 2;
+//            }else{
+//                return false;
+//            }
+//        }
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(isPowerOfFour(0));
+//        System.out.println(isPowerOfFour(1));
+//        System.out.println(isPowerOfFour(2));
+//        System.out.println(isPowerOfFour(3));
+//        System.out.println(isPowerOfFour(4));
+//        System.out.println(isPowerOfFour(5));
+//        System.out.println(isPowerOfFour(9));
+//
+//
+//    }
+//}
