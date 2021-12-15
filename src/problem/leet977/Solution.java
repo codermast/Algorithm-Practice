921a5cbee8571c54810e57c001ed0be2a40ef32c
小鹏 <981077525@qq.com>
2021-12-15 23:05:59 +0800

    Leetcode 977. 有序数组的平方

---
+++ src/problem/leet977/Solution.java
@@ -0,0 +1,13 @@
+package problem.leet977;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
