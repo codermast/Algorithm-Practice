24207d15630433f643a0ca4a72e851eae1292cf6
小鹏 <981077525@qq.com>
2021-12-19 23:26:09 +0800

    Leetcode 705. 设计哈希集合

---
+++ src/problem/leet705/MyHashSet.java
@@ -0,0 +1,31 @@
+package problem.leet705;
+
+import java.util.LinkedList;
+
+public class MyHashSet {
+
+    private LinkedList<Integer> list;
+
+    public MyHashSet() {
+        this.list = new LinkedList();
+    }
+
+    public void add(int key) {
+        if(!contains(key)){
+            list.add(key);
+        }
+    }
+
+    public void remove(int key) {
+        list.remove((Integer)key);
+    }
+
+    public boolean contains(int key) {
+        for (Integer integer : list) {
+            if(integer == key){
+                return true;
+            }
+        }
+        return false;
+    }
+}
