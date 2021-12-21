01d66ef72310b74b9b9ffd2f33be7da4d13b8162
小鹏 <981077525@qq.com>
2021-12-21 14:12:12 +0800

    Leetcode 1672. 最富有客户的资产总量

---
+++ src/problem/leet1672/Solution.java
@@ -0,0 +1,17 @@
+package problem.leet1672;
+
+public class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for(int[] arr : accounts){
+            int sum = 0;
+            for(int i : arr){
+                sum += i;
+            }
+            if(sum > max){
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
