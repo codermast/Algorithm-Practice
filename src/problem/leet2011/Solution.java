689064e45e25fcb1ed1f49e02a3eac145cd720fa
小鹏 <981077525@qq.com>
2021-12-21 14:09:31 +0800

    Leetcode 2011. 执行操作后的变量值

---
+++ src/problem/leet2011/Solution.java
@@ -0,0 +1,22 @@
+package problem.leet2011;
+
+public class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int i = 0;
+        for(String str : operations){
+            if(str.equals("++X")){
+                ++i;
+            }
+            if(str.equals("X++")){
+                i++;
+            }
+            if(str.equals("X--")){
+                i--;
+            }
+            if(str.equals("--X")){
+                --i;
+            }
+        }
+        return i;
+    }
+}
