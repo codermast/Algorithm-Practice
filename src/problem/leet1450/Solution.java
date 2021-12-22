5f6843f492fe88997a749970b3467e1c9232364a
小鹏 <981077525@qq.com>
2021-12-22 17:36:18 +0800

    Leetcode 1450. 在既定时间做作业的学生人数

--- src/problem/leet1450/Solution.java
+++ src/problem/leet1450/Solution.java
@@ -1,7 +1,7 @@
 package problem.leet1450;
 
 public class Solution {
-    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+    public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
         int retCount = 0;
         for(int i = 0;i<startTime.length;i++){
             if(queryTime >= startTime[i] && queryTime <= endTime[i]){
@@ -9,5 +9,11 @@ public class Solution {
             }
         }
         return retCount;
+    }
+
+    public static void main(String[] args) {
+        int[] startTime = {1,2,3};
+        int[] endTime = {3,2,7};
+        System.out.println(busyStudent(startTime, endTime, 4));
     }
 }
