bfb02ea1cc96b09f59962673bc94876e7fb3d613
小鹏 <981077525@qq.com>
2021-12-24 23:12:45 +0800

    Leetcode 1832. 判断句子是否为全字母句

---
+++ src/problem/leet1832/Solution.java
@@ -0,0 +1,23 @@
+package problem.leet1832;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length() < 26){
+            return false;
+        }
+
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            set.add(c);
+        }
+
+        return set.size() == 26;
+    }
+}
