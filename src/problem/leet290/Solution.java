0a2b4689e68d52a5546f1ce83ac6f3a089111e46
小鹏 <981077525@qq.com>
2021-12-24 10:53:47 +0800

    Leetcode 290. 单词规律

--- src/problem/leet290/Solution.java
+++ src/problem/leet290/Solution.java
@@ -25,16 +25,20 @@ public class Solution {
                 // 包含对应的 Value 值
                 Set<Map.Entry<Character, String>> entries = map.entrySet();
                 for (Map.Entry<Character, String> entry : entries) {
+                    // 遍历Map，查找对应 value 的 key 值
                     if(entry.getValue().equals(sarr[i])){
+                        // 如果对应value的key和实际的不一致，则false
                         if(!entry.getKey().equals(c)){
                             return false;
                         }
                     }
                 }
             } else{
+                // 不包含Key 和 value，则put进key-value键值对
                 map.put(c,sarr[i]);
             }
         }
+        // 如果i走到最后则说明已经判断完全了，可以返回true
         return i == pattern.length();
     }
 }
