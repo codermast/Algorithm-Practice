d877c3b153755e2c3f46966a0d4de0d2ffdd2336
小鹏 <981077525@qq.com>
2021-12-26 22:04:39 +0800

    剑指 Offer 42. 连续子数组的最大和

---
+++ src/SwordFingerOffer/offer42/Solution.java
@@ -0,0 +1,13 @@
+package SwordFingerOffer.offer42;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+}
