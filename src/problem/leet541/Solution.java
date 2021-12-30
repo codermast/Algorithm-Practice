072080a9ebb09e9884fdcf9e8b5ec0b2247fdb29
小鹏 <981077525@qq.com>
2021-12-30 23:23:36 +0800

    Leetcode 541. 反转字符串 II

--- src/problem/leet541/Solution.java
+++ src/problem/leet541/Solution.java
@@ -3,6 +3,33 @@ package problem.leet541;
 public class Solution {
 
     public String reverseStr(String s, int k) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        // 每次增加 2k 长度
+        for(int i = 0;i<s.length();i += k * 2){
+            myReverse(arr,i,i + k);
+        }
 
+        int surplus = n % (k * 2);
+
+        if(surplus < k){
+            myReverse(arr,n - surplus - 1,n);
+        }
+
+        if(surplus < k * 2 && surplus >= k){
+            myReverse(arr,n - surplus - 1,n - surplus - 1 + k);
+        }
+
+        return new String(arr);
+    }
+
+    private void myReverse(char[] arr, int start, int end) {
+        while (start < end){
+            char c = arr[start];
+            arr[start] = arr[end - 1];
+            arr[end - 1] = c;
+            start++;
+            end --;
+        }
     }
 }
