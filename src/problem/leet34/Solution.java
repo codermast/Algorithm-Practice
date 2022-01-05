238a0e357685eea633c3c630901ac45d4eb791f9
小鹏 <981077525@qq.com>
2022-01-05 23:16:28 +0800

    Leetcode 34. 在排序数组中查找元素的第一个和最后一个位置

--- src/problem/leet34/Solution.java
+++ src/problem/leet34/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
     public int[] searchRange(int[] nums, int target) {
         int length = nums.length;
         //给定初始值
+
         int [] ans = new int[]{-1,-1};
         for (int i = 0;i < length;i++){
             if(nums[i] == target){
