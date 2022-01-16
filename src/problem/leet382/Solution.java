8463cedfe2c8d22fec6fa7835e012fa8e5694e4b
小鹏 <981077525@qq.com>
2022-01-16 15:08:41 +0800

    LeetCode 382. 链表随机节点

---
+++ src/problem/leet382/Solution.java
@@ -0,0 +1,60 @@
+package problem.leet382;
+
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+
+    ListNode[] arr;
+    int size;
+
+    public Solution(ListNode head) {
+        List<ListNode> list = new LinkedList<>();
+
+        for(ListNode i = head;i != null;i = i.next) {
+            list.add(i);
+        }
+
+        this.arr =  list.toArray(new ListNode[list.size()]);
+        this.size = list.size();
+    }
+
+    public int getRandom() {
+        Random random = new Random();
+
+        int i = random.nextInt(size);
+
+        return arr[i].val;
+    }
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+
+        a.next = b;
+        b.next = c;
+
+        Solution s= new Solution(a);
+        System.out.println(s.getRandom());
+    }
+}
