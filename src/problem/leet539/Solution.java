f4db466e259f2c325617bf59542daa7470976e23
小鹏 <981077525@qq.com>
2022-01-18 22:46:08 +0800

    LeetCode 539. 最小时间差

--- src/problem/leet539/Solution.java
+++ src/problem/leet539/Solution.java
@@ -17,8 +17,11 @@ public class Solution {
             return 0;
         }
 
+        // 将所给的链表转换为数组
         String[] strArr = timePoints.toArray(new String[0]);
 
+        // 将数组进行排序，传入自定义比较器
+        // 将其按照字符串所代表的时间升序排列
         Arrays.sort(strArr, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
@@ -33,10 +36,14 @@ public class Solution {
             }
         });
 
+        // 最小时间差
+        // 一般求最小值时，给最大的默认值，反之则反
         int minTimeDif = Integer.MAX_VALUE;
+        // 遍历数组，求差
         for (int i = 0; i < strArr.length - 1; i++) {
             minTimeDif = Math.min(differ(strArr[i + 1], strArr[i]), minTimeDif);
         }
+        // 最后一个数和第一个数的差，并求最小值
         return Math.min(Math.abs(1440 - differ(strArr[strArr.length - 1], strArr[0])), minTimeDif);
 
     }
