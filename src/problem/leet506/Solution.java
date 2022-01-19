e4bf9673622553b8b0c2c736ea84d778d6fcd95b
小鹏 <981077525@qq.com>
2022-01-19 19:46:26 +0800

    LeetCode 506. 相对名次

---
+++ src/problem/leet506/Solution.java
@@ -0,0 +1,58 @@
+package problem.leet506;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution {
+    static class Node {
+        int num;
+        int index;
+
+        public Node(int num,int index) {
+            this.num = num;
+            this.index = index;
+        }
+    }
+
+    public String[] findRelativeRanks(int[] score) {
+        String[] ret = new String[score.length];
+
+        PriorityQueue<Node> queue = new PriorityQueue<>(score.length, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o2.num - o1.num;
+            }
+        });
+
+        for (int i = 0;i<score.length;i++) {
+            queue.add(new Node(score[i],i));
+        }
+
+        int ranking = 1;
+        while (!queue.isEmpty()){
+            Node poll = queue.poll();
+            ret[poll.index] = getInfor(ranking);
+            ranking++;
+        }
+        return ret;
+    }
+
+    private String getInfor(int ranking) {
+        switch(ranking){
+            case 1:return "Gold Medal";
+            case 2:return "Silver Medal";
+            case 3:return "Bronze Medal";
+            default:return String.valueOf(ranking);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s= new Solution();
+        int[] arr = {5,4,3,2,1};
+        int[] arr2 = {10,3,8,9,4};
+
+        System.out.println(Arrays.toString(s.findRelativeRanks(arr)));
+        System.out.println(Arrays.toString(s.findRelativeRanks(arr2)));
+    }
+}
