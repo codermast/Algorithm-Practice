452c32f04b4d7cfd4e9f850d8160b6669561f5a6
小鹏 <981077525@qq.com>
2022-02-11 21:22:44 +0800

    LeetCode 1984. 学生分数的最小差值

--- src/problem/leet1984/Solution.java
+++ src/problem/leet1984/Solution.java
@@ -4,13 +4,16 @@ import java.util.Arrays;
 
 public class Solution {
     public static int minimumDifference(int[] nums, int k) {
+        // 将数组进行排序
         Arrays.sort(nums);
 
+        // 如果只选择一个数字，则最大最小值的差为0
         if(k == 1){
             return 0;
         }
 
         int min = Integer.MAX_VALUE;
+        // 滑动窗口解决
         for (int i = 0; i + k- 1 < nums.length; i++) {
             min = Math.min((nums[i + k - 1] - nums[i]),min);
         }
