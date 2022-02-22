9ded2a95f8af84dd31111b567d56c1840d4d1525
小鹏 <981077525@qq.com>
2022-02-22 10:41:56 +0800

    LeetCode 509. 斐波那契数

--- src/problem/leet509/Solution.java
+++ src/problem/leet509/Solution.java
@@ -36,9 +36,31 @@ public class Solution {
         return dp[n];
     }
 
+    // 动态规划法2：仅维护两个需要递推的数值，不需要记录整个dp数组（优化）
+    public int fib3(int n) {
+        if (n < 2) {
+            return n;
+        }
+        // 定义 dp 数组
+        int[] dp = new int[2];
+
+        // dp 数组的初始化
+        // dp[0] = 0; int 数组初始化会将所有的元素默认初始化为 0
+        dp[1] = 1;
+
+        // 循环 n - 1 次即可得到第 n 个斐波那契数
+        for (int i = 0; i < n - 1; i++) {
+            int num = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = num;
+        }
+        return dp[1];
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(s.fib(4));
         System.out.println(s.fib2(4));
+        System.out.println(s.fib3(4));
     }
 }
