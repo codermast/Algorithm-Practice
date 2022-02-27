7e602e44460d8e217c29f5f69b922851d745afda
小鹏 <981077525@qq.com>
2022-02-27 11:06:10 +0800

    Leetcode 6008. 统计包含给定前缀的字符串

---
+++ Leetcode/src/leet6008/Solution.java
@@ -0,0 +1,27 @@
+package leet6008;
+
+public class Solution {
+    public static int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for (String word : words) {
+            if (word.length() < pref.length()){
+                continue;
+            }
+            String substring = word.substring(0, pref.length());
+            if (substring.equals(pref)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"pay","attention","practice","attend"};
+        String[] arr2 = {"leetcode","win","loops","success"};
+        String[] arr3 = {"cullp","ypfyqcljk","jtuogvscob","dsriyigc","fr","jtuogvscou","doivwcgxpo","jtuogvscob","chuiw","fqdhyhkvtz","cszrtrff","kssjy","ps","xvax","vekbmwwnra"};
+        System.out.println(prefixCount(arr, "at"));
+        System.out.println(prefixCount(arr2, "code"));
+        System.out.println(prefixCount(arr3,"jtuogvsco"));
+
+    }
+}
