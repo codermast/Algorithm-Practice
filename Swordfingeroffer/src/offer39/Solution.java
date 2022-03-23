3175a138302b0a4ee706d539a0dc302cb3d79b89
小鹏 <981077525@qq.com>
2022-03-23 10:47:59 +0800

    剑指 Offer 39. 数组中出现次数超过一半的数字

---
+++ Swordfingeroffer/src/offer39/Solution.java
@@ -0,0 +1,10 @@
+package offer39;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
