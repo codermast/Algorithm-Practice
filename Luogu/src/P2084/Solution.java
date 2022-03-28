0fc139fc672665d79376956069e42f65c8ec1ec2
小鹏 <981077525@qq.com>
2022-03-28 20:05:01 +0800

    Luogu P2084 进制转换

---
+++ Luogu/src/P2084/Solution.java
@@ -0,0 +1,29 @@
+package P2084;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int m = scanner.nextInt();
+
+        String n = String.valueOf(scanner.next());
+
+        StringBuilder ret = new StringBuilder();
+
+        for (int i = 0; i < n.length(); i++) {
+            int num = Integer.parseInt(String.valueOf(n.charAt(i)));
+            if (num != 0) {
+                ret.append(num).append("*").append(m).append("^").append(n.length() - i - 1);
+                if (i != n.length() - 1) {
+                    ret.append("+");
+                }
+            }
+        }
+        if (ret.charAt(ret.length() - 1) == '+'){
+            ret.delete(ret.length() - 1,ret.length());
+        }
+        System.out.println(ret);
+    }
+}
