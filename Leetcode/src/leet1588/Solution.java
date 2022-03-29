34dcf33506549fc47cdd460deaad6e3561eac246
小鹏 <981077525@qq.com>
2022-03-29 11:20:59 +0800

    Leetcode 1588. 所有奇数长度子数组的和

--- Leetcode/src/leet1588/Solution.java
+++ Leetcode/src/leet1588/Solution.java
@@ -1,37 +1,22 @@
 package leet1588;
 
 public class Solution {
-    public static int sumOddLengthSubarrays(int[] arr) {
-        // 返回值
-        int ret = 0;
-        // 步长
-        int arrlen = 1;
-
-        // 步长 <= 数组长度时即可开始循环
-        while(arrlen <= arr.length){
-            // 左索引
-            int leftIndex = 0;
-            // 右索引
-            int rightIndex = leftIndex + arrlen;
-            // 只有当右索引小于数组长度时
-            while (rightIndex < arr.length) {
-                int sum = 0;
-
-                for (int i = leftIndex; i < rightIndex; i++) {
-                    sum += arr[i];
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+        int len = 1;
+        while (len <= arr.length) {
+            for (int i = 0; i <= arr.length - len; i++) {
+                for (int j = i; j < len + i; j++) {
+                    sum += arr[j];
                 }
-                ret += sum;
-
-                leftIndex ++;
-                rightIndex ++;
             }
-
-            arrlen += 2;
+            len += 2;
         }
-        return ret;
+        return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumOddLengthSubarrays(new int[]{1,4,2,5,3}));
+        Solution s = new Solution();
+        System.out.println(s.sumOddLengthSubarrays(new int[]{1, 4, 2, 5, 3}));
     }
 }
