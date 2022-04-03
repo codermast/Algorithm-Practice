c0c4e6343e38964db5de27195ae7bc3dfd25987f
小鹏 <981077525@qq.com>
2022-04-03 10:22:28 +0800

    Leetcode 104. 二叉树的最大深度(迭代法)

---
+++ Leetcode/src/leet104/Solution2.java
@@ -0,0 +1,53 @@
+package leet104;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution2 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 深度优先遍历迭代法
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int maxLen = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                assert poll != null;
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+            // 在遍历完最后一层后，是会再进行 maxLen ++，所以初始的时候的 maxLen初始化为 0
+            maxLen++;
+        }
+        return maxLen;
+    }
+}
