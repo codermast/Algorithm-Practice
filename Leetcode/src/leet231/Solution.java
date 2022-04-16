44e950b53031e74456a105d9a5d1db40904f9685
小鹏 <981077525@qq.com>
2022-04-16 15:39:32 +0800

    Leetcode 231. 2 的幂

--- Leetcode/src/leet231/Solution.java
+++ Leetcode/src/leet231/Solution.java
@@ -1,17 +1,16 @@
 package leet231;
 
 public class Solution {
-
     // 迭代法
     public boolean isPowerOfTwo(int n) {
-        if(n == 1){
+        if (n == 1) {
             return true;
         }
         long num = 1;
-        while (num <= n){
-            if(num == n){
+        while (num <= n) {
+            if (num == n) {
                 return true;
-            }else{
+            } else {
                 num *= 2;
             }
         }
@@ -20,12 +19,17 @@ public class Solution {
 
     // 递归法 : 栈溢出的可能
     public boolean isPowerOfTwo2(int n) {
-        if (n == 1){
+        if (n == 1) {
             return true;
         }
-        if (n % 2 != 0){
+        if (n % 2 != 0) {
             return false;
         }
         return isPowerOfTwo2(n / 2);
+    }
+
+    // 判断二进制中 1 的个数的方法
+    public boolean isPowerOfTwo3(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
