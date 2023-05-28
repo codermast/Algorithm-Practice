0bbfafb3a4cb8c526b87c3e1dc51e321eff48686
codermast <codermast@qq.com>
2023-05-28 17:18:18 +0800

    Leetcode 112. 路径总和

--- Leetcode/src/leet112/Solution.java
+++ Leetcode/src/leet112/Solution.java
@@ -1,7 +1,5 @@
 package leet112;
 
-import java.util.Stack;
-
 class Solution {
 
     static class TreeNode {
@@ -23,30 +21,16 @@ class Solution {
         }
     }
 
-        public boolean hasPathSum(TreeNode root, int targetSum) {
-            if(root == null){
-                return false;
-            }
-            Stack<TreeNode> stack = new Stack<>();
-            int sum = 0;
-            // 非递归遍历 : 前序
-            while(!stack.empty()){
-                Stack<TreeNode> path = new Stack<>();
-                TreeNode node = root;
-                while(node != null){
-                    path.push(node);
-                    sum += node.val;
-                    node = node.left;
-                }
-
-                path.pop();
-                node = path.peek().right;
-
-                if(sum == targetSum){
-                    return true;
-                }
-
-            }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        // 树为空时，则必定为 false
+        if (root == null) {
             return false;
         }
+
+        if (root.left != null && root.right != null && root.val == targetSum){
+            return true;
+        }
+
+        return (hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val));
+    }
 }
