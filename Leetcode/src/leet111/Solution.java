827a756375df5efddd3d5ff6e3112faf43be5d5c
codermast <codermast@qq.com>
2023-09-06 21:26:03 +0800

    Leetcode 111. 二叉树的最小深度

--- Leetcode/src/leet111/Solution.java
+++ Leetcode/src/leet111/Solution.java
@@ -1,5 +1,8 @@
 package leet111;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
     static class TreeNode {
         int val;
@@ -39,5 +42,37 @@ public class Solution {
             minLen = Math.min(minLen, minDepth(root.right));
         }
         return minLen + 1;
+    }
+
+    // 遍历迭代法，广度优先
+    public int minDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int minLen = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+
+                if (poll.left == null && poll.right == null) {
+                    return minLen + 1;
+                }
+
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+            minLen++;
+        }
+        return minLen;
     }
 }
