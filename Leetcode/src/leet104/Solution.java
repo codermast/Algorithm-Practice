c5d5ccf40da848db154058148d7d34152263958f
codermast <codermast@qq.com>
2023-09-06 19:24:14 +0800

    Leetcode 104. 二叉树的最大深度

--- Leetcode/src/leet104/Solution.java
+++ Leetcode/src/leet104/Solution.java
@@ -1,5 +1,8 @@
 package leet104;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
     static class TreeNode {
@@ -32,5 +35,35 @@ public class Solution {
             return 0;
         }
         return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+    }
+
+    // 深度优先遍历迭代法
+    // 最大的深度就是二叉树层序遍历的层数，所以这里我们使用层序遍历
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // 层序遍历需要借助 队列 来实现
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int maxLen = 0;
+        while (!queue.isEmpty()) {
+            // size 记录该层的节点数
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                assert poll != null;
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+            // 在遍历完最后一层后，是会再进行 maxLen ++，所以初始的时候的 maxLen初始化为 0
+            maxLen++;
+        }
+        return maxLen;
     }
 }
