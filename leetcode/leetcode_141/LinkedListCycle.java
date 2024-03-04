1dc172c35176c3cc660ebb62ef4b46b51e57ea52
codermast <codermast@qq.com>
2024-03-04 11:45:55 +0800

    Leetcode 141. 环形链表

---
+++ leetcode/leetcode_141/LinkedListCycle.java
@@ -0,0 +1,30 @@
+package leetcode_141;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode fast = head.next, slow = head;
+
+        while (fast != null && fast.next != null) {
+            if (fast == slow) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+}
