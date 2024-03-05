f87bab6d1b0fc6eb26803812d7eb31dfd5bc77a5
codermast <codermast@qq.com>
2024-03-05 09:13:48 +0800

    Leetcode 283. 移动零

---
+++ leetcode/leetcode_283/MoveZeroes.java
@@ -0,0 +1,17 @@
+package leetcode_283;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] != 0) {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
