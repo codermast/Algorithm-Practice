027ab7f4f98fa0bd97d7d6d2a9796fd0ee9fdb0b
codermast <codermast@qq.com>
2024-03-05 09:24:38 +0800

    Leetcode 283. 移动零

--- leetcode/leetcode_283/MoveZeroes.java
+++ leetcode/leetcode_283/MoveZeroes.java
@@ -14,4 +14,24 @@ public class MoveZeroes {
             }
         }
     }
+
+    // 双指针：单次遍历
+    public void moveZeroes1(int[] nums) {
+        if (nums == null || nums.length == 1) {
+            return;
+        }
+
+        // left 确定已排序的末尾，right 确定需要交换的非零元素
+        int left = 0, right = 0;
+
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+                left++;
+            }
+            right++;
+        }
+    }
 }
