8fd428188318c916c901401dd4574cc79f57dec4
友人 <codermast@qq.com>
2024-08-29 15:05:10 +0800

    Leetcode 455. 分发饼干

--- leetcode/leetcode_455/FindContentChildren.java
+++ leetcode/leetcode_455/FindContentChildren.java
@@ -3,7 +3,8 @@ package leetcode_455;
 import java.util.Arrays;
 
 public class FindContentChildren {
-    // 解题思路：用最大的饼干去满足尽可能大胃口的孩子，以避免饼干尺寸的浪费
+    // 解题思路  1. 用最大的饼干去满足尽可能大胃口的孩子，以避免饼干尺寸的浪费。
+    //          2. 也可以使用最小的饼干，优先满足小胃口的孩子。
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
