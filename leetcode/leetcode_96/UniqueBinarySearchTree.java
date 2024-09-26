2ff69737e392721db3d8b9604c21cdd9106560d8
友人 <codermast@qq.com>
2024-09-27 00:29:14 +0800

    Leetcode 96. 不同的二叉搜索树

--- leetcode/leetcode_96/UniqueBinarySearchTree.java
+++ leetcode/leetcode_96/UniqueBinarySearchTree.java
@@ -2,6 +2,25 @@ package leetcode_96;
 
 public class UniqueBinarySearchTree {
 
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
 
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            // i 为几个节点时，dp[i] 为 i 个结点的时候，不同二叉搜索树的个数
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        UniqueBinarySearchTree instance = new UniqueBinarySearchTree();
+
+        System.out.println(instance.numTrees(3));
+    }
 
 }
